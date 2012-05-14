5ec220db94620f3a9a923c6804909abb2c4e8d6b
waynegerard <WayneGerard@gmail.com>
2012-05-14 12:34:41 -0700

    updated prez.js to handle cases more efficiently

--- app/assets/javascripts/prez.js
+++ app/assets/javascripts/prez.js
@@ -1,9 +1,55 @@
-$(document).ready(function() {
+/**
+	slidePoll()
+ 	Polling function. Polls every |timeout| milliseconds to see if the active slide has changed. If so, changes the slide to the appropriate one.
+*/
+function slidePoll() {
+	var slideContainer 	= $("#slideshow");	
+ 	var poll_url 		= slideContainer.attr('data-poll-url');
+	var active_key 		= 'active';
+	var timeout         = 5000;
+	
+    $.get(poll_url,
+    function(data) {
+
+		// Default in case no data yet
+		if (!data) {
+			data = { active: '1' }
+		} 
+
+        // Get the active slide from JSON and on the page
+        var active_slide = data[active_key];
+        var active_li = $("li.active").text();
+
+        // If different, change using the click handler
+        if (active_li != active_slide) {
+            var query = "li:contains(" + active_slide + ")";
+            $(query).click();
+        }
+    });
+    setTimeout("slidePoll()", timeout);
+}
+
+/**
+	setSlide()
+	Sets the slide, server-side. Checks for appropriate permissions first.
+*/
+function setSlide() {
+	var admin     = $("#admin").size() > 0;
+	var active_li = $("li.active").text();
+	var data      = { active: active_li };
+	var post_url  = $("#slideshow").attr('data-post-url');
+	if (!admin) {
+		return;
+	}
+	
+	$.post(post_url, data);	
+}
+
+$(window).load(function() {
 
 	///////////
 	// ORBIT //
 	///////////
-	
 	var slideContainer 	= $("#slideshow");
     slideContainer.orbit({
         animation: 'fade',
@@ -17,55 +63,50 @@ $(document).ready(function() {
 	//////////
 	
     var admin 			= $("#admin").size() > 0;
-    var poll_url 		= slideContainer.attr('data-poll-url');
-	var post_url 		= slideContainer.attr('data-post-url');
 	var orbit_list 		= $("ul.orbit-bullets");
-    var active_key 		= 'active';
-
-
-	// Polling function
-    function slidePoll() {
-        $.GET(poll_url,
-        function(data) {
-
-			// Default in case no data yet
-			if (!data) {
-				data = { active: '1' }
-			} 
-
-            // Get the active slide from JSON and on the page
-            var active_slide = data[active_key];
-            var active_li = $("li.active").text();
-
-            // If different, change using the click handler
-            if (active_li != active_slide) {
-                var query = "li:contains(" + active_slide + ")"
-                $(query).click();
-            }
-        });
-        setTimeout("slidePoll()", 500);
-    }
-
+	var slider_nav      = $(".slider-nav");
     // Hide bullets for non-admins
     if (!admin) {
-        $("ul.orbit-bullets").css('display: none');
+        $("ul.orbit-bullets").css('display', 'none');
+		$(".slider-nav").css('display', 'none');
     }
 
-	// Admin posting to change slide server-wide
-	if (admin) {
+	if (admin) {	
+
+		// Click handler for bullets
 		var bullets = $("ul.orbit-bullets").children();
 		$.each(bullets, function(index, value) {
 			
 			$(value).click(function(e) {
-				var active_li = $("li.active").text();
-				var data = { active: active_li };
-				$.POST(post_url, data, function());
+				setSlide();
 			});
 			
 		});
+		
+		// Click handler for nav arrows
+		$.each(slider_nav.children(), function(index, value) {
+			$(value).click(function(e) {
+				setSlide();
+			})
+		});
+		
+		// Key navigation
+		$(document).keydown(function(e) {
+			if (event.which == 37) { // left key
+				slider_nav.children(".left").click();
+				setSlide();
+			}
+			else if (event.which == 39) { // right key
+				slider_nav.children(".right").click();
+				setSlide();
+			}
+		});
+		
 	}
 	
 	// Start polling
-	slidePoll();
+	if (!admin) {
+		slidePoll();
+	}
 
 });
