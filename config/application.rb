bf2ca51364f523257cdb3e2ba6394fa63c8699a0
waynegerard <WayneGerard@gmail.com>
2012-05-14 16:19:18 -0700

    updated application file for heroku

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,8 @@ module Prez
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
